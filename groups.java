a6b1a95e5b484e6ead5b37cea42ec6de602c9255
anui12 <137377499+anui12@users.noreply.github.com>
2023-11-12 13:29:05 +0530

    Add files via upload

---
+++ groups.java
@@ -0,0 +1,32 @@
+package ParellelExecution;
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.Test;
+
+	public class groups {
+
+		@Test
+		public void facebook() {
+		WebDriver d=new ChromeDriver();
+		d.get("https://www.facebook.com/");
+		}
+		@Test(groups="Shopping")
+		public void flipkart() {
+			WebDriver d=new ChromeDriver();
+			d.get("https://www.flipkart.com/");
+		}
+		@Test(groups="Shopping")
+		public void myntra() {
+			WebDriver d=new ChromeDriver();
+			d.get("https://www.myntra.com/shop/men");
+		}
+		@Test
+		public void instagram() {
+			WebDriver d=new ChromeDriver();
+			d.get("https://www.instagram.com/direct/inbox/");
+		}
+	}
+
+
